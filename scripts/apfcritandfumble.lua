b26bd459071ce3c76b1bdffe131bfb1614cc6051
Wil Thieme <wil.thieme@protonmail.com>
2022-07-21 20:25:54 -0400

    allow imprecise weapon name search

--- scripts/apfcritandfumble.lua
+++ scripts/apfcritandfumble.lua
@@ -21,15 +21,18 @@ local function kelSpell(rRoll)
 end
 
 -- Check if Advanced Effects is loaded and whether the attack has a Weapon attached
-local function advEffectsSpell(rRoll)
-	return AdvancedEffects and not rRoll.nodeWeapon
+local function advEffectsSpell(rSource, rRoll)
+	return AdvancedEffects and not rSource.nodeWeapon
 end
 
 -- Determine attack type
-local function attackType(rRoll)
-	if DataCommon.naturaldmgtypes[getWeaponName(rRoll.sDesc):lower()] then
-		return "Natural";
-	elseif kelSpell(rRoll) or advEffectsSpell(rRoll) then
+local function attackType(rSource, rRoll)
+	for kDmgType, _ in pairs(DataCommon.naturaldmgtypes) do
+		if string.find(getWeaponName(rRoll.sDesc):lower(), kDmgType) then
+			return "Natural";
+		end
+	end
+	if kelSpell(rRoll) or advEffectsSpell(rSource, rRoll) then
 		return "Magic";
 	elseif string.match(rRoll.sDesc, "%[ATTACK.*%((%w+)%)%]") == "R" then
 		return "Ranged";
@@ -39,27 +42,35 @@ local function attackType(rRoll)
 end
 
 -- Determine damage type
-local function damageType(rRoll)
+local function damageType(rSource, rRoll)
 	local sWeapon = getWeaponName(rRoll.sDesc);
-	if DataCommon.naturaldmgtypes[sWeapon:lower()] then
-		return DataCommon.naturaldmgtypes[sWeapon:lower()]:gsub(',.*', '');
-	elseif DataCommon.weapondmgtypes[sWeapon:lower()] then
-		return DataCommon.weapondmgtypes[sWeapon:lower()]:gsub(',.*', '');
-	elseif kelSpell(rRoll) or advEffectsSpell(rRoll) then
+	local aDmgTypes = { DataCommon.naturaldmgtypes, DataCommon.weapondmgtypes };
+	for _, tDmgTypes in pairs(aDmgTypes) do
+		for kDmgType, vDmgType in pairs(tDmgTypes) do
+			if string.find(sWeapon:lower(), kDmgType) then
+				if type(vDmgType) == 'string' then
+					return vDmgType:gsub(',.*', '');
+				else
+					return vDmgType['*'];
+				end
+			end
+		end
+	end
+	if kelSpell(rRoll) or advEffectsSpell(rSource, rRoll) then
 		return "Magic";
 	else
 		return 'bludgeoning'
 	end
 end
 
-local function onPostAttackResolve_new(_, _, rRoll)
+local function onPostAttackResolve_new(rSource, _, rRoll)
 	-- HANDLE FUMBLE/CRIT HOUSE RULES
 	local sOptionHRFC = OptionsManager.getOption("HRFC");
 	if rRoll.sResult == "fumble" and ((sOptionHRFC == "both") or (sOptionHRFC == "fumble")) then
-		AutoPFCritFumbleOOB.notifyApplyHRFC('Fumble - ' .. attackType(rRoll));
+		AutoPFCritFumbleOOB.notifyApplyHRFC('Fumble - ' .. attackType(rSource, rRoll));
 	end
 	if rRoll.sResult == "crit" and ((sOptionHRFC == "both") or (sOptionHRFC == "criticalhit")) then
-		AutoPFCritFumbleOOB.notifyApplyHRFC('Critical - ' .. StringManager.titleCase(damageType(rRoll)));
+		AutoPFCritFumbleOOB.notifyApplyHRFC('Critical - ' .. StringManager.titleCase(damageType(rSource, rRoll)));
 	end
 end
 
